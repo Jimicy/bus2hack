6dc141ae36899fc3963b9c4767724e18985daa87
Jimmy Wang <Jimmy@dyn129-100-133-148.eduroam.uwo.ca>
2015-03-28 21:52:54 -0400

    Add warden and authentication for a session.

--- db/migrate/20150328072104_create_user.rb
+++ db/migrate/20150328072104_create_user.rb
@@ -1,6 +1,8 @@
 class CreateUser < ActiveRecord::Migration
   def change
     create_table :users do |t|
+      t.string :email
+      t.string :password
       t.string :first_name
       t.string :last_name
       t.string :school
