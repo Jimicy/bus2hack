e12c5e085f595b8c2708a27668b0d39d4d40b0b9
Arya Solhi <aryasolhi@gmail.com>
2015-03-28 21:48:35 -0400

    fb login with angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,19 @@
+ window.fbAsyncInit = function() {
+        FB.init({
+          appId      : 'your-app-id',
+          xfbml      : true,
+          version    : 'v2.3'
+        });
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+
 var BusToHack = angular.module('BusToHack',['ngRoute'])
 
 BusToHack.config(['$routeProvider',
@@ -13,14 +29,17 @@ BusToHack.config(['$routeProvider',
   }])
 
 BusToHack.controller('loginCtrl', ["$scope", function($scope){
+    
     FB.getLoginStatus(function(response) {
         statusChangeCallback(response);
     });
     
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
     $scope.login = function(){
-        
-    }
-    
+        FB.login();
+    } 
 }])
 
 BusToHack.controller('mainCtrl',["$scope", function($scope){
